69e14154d27e7914e744a448762449018938a452
shivam003a <shivam003a@gmail.com>
2023-02-08 17:05:22 +0530

    10 commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Contact = () => {
-    const cText = document.querySelectorAll('.c-text');
+    const [formVisible, setFormVisible] = useState(false)
 
     const handleContact = ()=>{
+        const cText = document.querySelectorAll('.c-text');
         const toglePoint = window.innerHeight /5 * 3;
-
         cText.forEach((c)=>{
             const cTop = c.getBoundingClientRect().top;
-
-            if(cTop < toglePoint){
-                c.classList.add('animate')
-            }
-            else{
-                c.classList.remove('animate')
-            }
+            setFormVisible(cTop < toglePoint);
         })
-
-
     }
 
     useEffect(()=>{
@@ -31,12 +23,12 @@ const Contact = () => {
             <section className="contact">
                 <h1>Contact</h1>
                 <div className="contact-sub-div">
-                    <div className="contact-desc c-text">
+                    <div className={formVisible?"contact-desc c-text animate":"contact-desc c-text"}>
                         <span>At Earthly, we're here to help with any questions or concerns you may have about The Sustainable Kit. Whether you need help with your order, have a suggestion for us, or just want to say hello, we're always happy to hear from you.</span>
                         <span>Please don't hesitate to reach out to us by phone number +91-4363646754. Our customer service team is available from 9am to 5pm +5:30 GMT and will get back to you as soon as possible.</span>
                         <span>Thank you for choosing Earthly and for taking a step towards a more sustainable future!</span>
                     </div>
-                    <div className="contact-form c-text">
+                    <div className={formVisible?"contact-form c-text animate":"contact-form c-text"}>
                         <form>
                             <input type="text" placeholder="Full Name" />
                             <input type="email" placeholder="Email" />
--- src/components/Why.js
+++ src/components/Why.js
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Why = ()=>{
+    const [isTextVisible, setTextVisible] = useState(false);
 
     const handleTextAnimate = ()=>{
         const text = document.querySelectorAll('.txt')
@@ -8,13 +9,7 @@ const Why = ()=>{
 
         text.forEach((cont)=>{
             const top = cont.getBoundingClientRect().top;
-        
-            if(top < toggle){
-                cont.classList.add('animate');
-            }
-            else{
-                cont.classList.remove('animate');
-            }
+            setTextVisible(top < toggle)
         })
 
     }
@@ -27,10 +22,10 @@ const Why = ()=>{
     return (
         <>
             <section className="why">
-                <h1 className="txt">Why Us ?</h1>
-                <p className="txt">At Earthly, we believe that every small step towards sustainability can make a big difference. That's why we've created The Sustainable Kit, a collection of 5 essential products that make it easy for you to live a more eco-friendly life.Our commitment to sustainability goes beyond just the products we sell. We source materials ethically and make sure that our production process has minimal impact on the environment.</p>
-                <p className="txt">We stand behind the quality of our products and are confident that you'll love them. If you're not completely satisfied, we offer a 100% satisfaction guarantee.We're here to help and are always available to answer any questions you may have. Our customer service team is dedicated to providing you with the best possible experience, and we're confident that you'll love The Sustainable Kit as much as we do.</p>
-                <p className="txt">Join us on our mission to create a more sustainable future, and trust that The Sustainable Kit is the right choice for you</p>
+                <h1>Why Us ?</h1>
+                <p className={isTextVisible?"txt animate":"txt"}>At Earthly, we believe that every small step towards sustainability can make a big difference. That's why we've created The Sustainable Kit, a collection of 5 essential products that make it easy for you to live a more eco-friendly life.Our commitment to sustainability goes beyond just the products we sell. We source materials ethically and make sure that our production process has minimal impact on the environment.</p>
+                <p className={isTextVisible?"txt animate":"txt"}>We stand behind the quality of our products and are confident that you'll love them. If you're not completely satisfied, we offer a 100% satisfaction guarantee.We're here to help and are always available to answer any questions you may have. Our customer service team is dedicated to providing you with the best possible experience, and we're confident that you'll love The Sustainable Kit as much as we do.</p>
+                <p className={isTextVisible?"txt animate":"txt"}>Join us on our mission to create a more sustainable future, and trust that The Sustainable Kit is the right choice for you</p>
             </section>
         </>
     )
