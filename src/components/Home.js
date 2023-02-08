cedb2247b7080a9103d0d0be244d505cc8844af5
shivam003a <shivam003a@gmail.com>
2023-02-08 13:16:37 +0530

    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,8 +19,10 @@ const Home = () => {
                 <span className="overlay overlay-2" style={{ left: 2 * offset + "px", top: 2.3 * offset + "px" }}>affordable</span>
                 <span className="overlay overlay-3" style={{ left: (1.9 * offset - 200) + "px" }}>nature friendly</span>
                 <span className="overlay overlay-4" style={{ left: (3 * offset - 200) + "px" }}>reusable</span>
-                <h1 style={{ top: (parseInt(height/2.4) + 0.5 * offset) + "px" }}>Elevate Your Lifestyle with Sustainable Kit</h1>
-                <p style={{ top: (parseInt(height/1.98) + 0.5 * offset) + "px" }}>A collection of eco-friendly products that simplify your daily routine</p>
+                <div style={{ top: (parseInt(height / 2.4) + 0.5 * offset) + "px" }}>
+                    <h1 style={{ top: (parseInt(height / 2.4) + 0.5 * offset) + "px" }}>Elevate Your Lifestyle with Sustainable Kit</h1>
+                    <p style={{ top: (parseInt(height / 1.98) + 0.5 * offset) + "px" }}>A collection of eco-friendly products that simplify your daily routine</p>
+                </div>
             </section>
         </>
     )
